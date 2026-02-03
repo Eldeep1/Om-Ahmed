475fc93d6e4b298de240c1bdfd9c5e7436f9b716
Eldeep1 <alieldeep111@gmail.com>
2026-02-03 20:08:15 +0200

    finishing Auth GUI

--- app/src/main/java/com/depogramming/omahmed/presentation/Authentication/login/view/LoginFragment.java
+++ app/src/main/java/com/depogramming/omahmed/presentation/Authentication/login/view/LoginFragment.java
@@ -3,19 +3,25 @@ package com.depogramming.omahmed.presentation.Authentication.login.view;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.depogramming.omahmed.R;
 
 public class LoginFragment extends Fragment {
 
+    TextView singUpButton;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fragment_login, container, false);
+        View view = inflater.inflate(R.layout.fragment_login, container, false);
+        singUpButton=view.findViewById(R.id.signUpTextAction);
+        singUpButton.setOnClickListener(view1 -> Navigation.findNavController(view).navigate(R.id.action_loginFragment_to_registerFragment));
+        return view;
     }
 }
