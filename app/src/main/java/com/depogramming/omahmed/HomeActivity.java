f830ecf8ee9d5930b920fd9997315a77dd86278b
Eldeep1 <alieldeep111@gmail.com>
2026-02-06 14:07:28 +0200

    home static design

--- app/src/main/java/com/depogramming/omahmed/HomeActivity.java
+++ app/src/main/java/com/depogramming/omahmed/HomeActivity.java
@@ -32,7 +32,7 @@ public class HomeActivity extends AppCompatActivity {
         ViewCompat.setOnApplyWindowInsetsListener(binding.getRoot(), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
 
-            v.setPadding(systemBars.left, 0, systemBars.right, 0);
+            v.setPadding(systemBars.left,systemBars.top, systemBars.right, 0);
 
             return insets;
         });
