b087bf185e7b36015600585ec8c1814d4a6c2832
Eldeep1 <alieldeep111@gmail.com>
2026-02-17 08:18:50 +0200

    remove some logs and enhance image size

--- app/src/main/java/com/depogramming/omahmed/presentation/splash/presenter/SplashPresenterImp.java
+++ app/src/main/java/com/depogramming/omahmed/presentation/splash/presenter/SplashPresenterImp.java
@@ -2,7 +2,6 @@ package com.depogramming.omahmed.presentation.splash.presenter;
 
 
 import android.content.Context;
-import android.util.Log;
 
 import com.depogramming.omahmed.data.auth.repository.AuthRepo;
 import com.depogramming.omahmed.data.onboarding.repository.UserPreferencesRepository;
@@ -27,28 +26,13 @@ public class SplashPresenterImp implements SplashPresenter {
     public void decideNextScreen() {
         boolean finished = userPreferencesRepository.getOnBoardingFlag();
         if (finished) {
-            //TODO: CompositeDisposable again...
             Disposable subscribe = authRepo.getRegisteredUser()
                     .subscribeOn(Schedulers.io())
                     .observeOn(AndroidSchedulers.mainThread())
-                    .doOnSubscribe(d -> Log.d("RxDebug", "Started checking user..."))
-                    .doOnTerminate(() -> Log.d("RxDebug", "Chain terminated"))
                     .subscribe(
-                            firebaseUser -> {
-                                Log.d("RxDebug", "User found: " + firebaseUser.getUid());
-                                Log.d("RxDebug", "User found: " + firebaseUser.getEmail());
-                                Log.d("RxDebug", "User found: " + firebaseUser.getDisplayName());
-                                splashView.navigateToHome();
-                            },
-                            throwable -> {
-                                Log.e("RxDebug", "Error occurred", throwable);
-                                splashView.navigateToLogin();
-                            },
-                            () -> {
-                                // This runs if the Observable completes WITHOUT emitting a user
-                                Log.d("RxDebug", "Completed with NO user (Empty)");
-                                splashView.navigateToLogin();
-                            }
+                            firebaseUser -> splashView.navigateToHome(),
+                            throwable -> splashView.navigateToLogin(),
+                            () -> splashView.navigateToLogin()
                     );
         } else {
             splashView.navigateToOnBoarding();
